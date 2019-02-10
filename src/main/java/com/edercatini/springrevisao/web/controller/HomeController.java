ca2e10f9c4ef18297a6ef911eb2e6ea1d9bedd66
Eder Catini <edergcatini@icloud.com>
2019-02-10 14:25:59 -0200

    Inclusão de recursos e criação de Controller para a view Home

---
+++ src/main/java/com/edercatini/springrevisao/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.edercatini.springrevisao.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "/home";
+	}
+}
