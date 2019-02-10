0c6e5dbb40486879aa62b9ff1b8adff9562257ba
Eder Catini <edergcatini@icloud.com>
2019-02-10 14:02:36 -0200

    Estrutura inicial do projeto

---
+++ src/main/java/com/edercatini/springrevisao/ImproveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.edercatini.springrevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveSpringBootApplication.class, args);
+	}
+
+}
