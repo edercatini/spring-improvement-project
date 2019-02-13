e18757df73db176a3ec00dcaddfe8782f59dba21
Eder Catini <edergcatini@icloud.com>
2019-02-12 22:54:25 -0200

    Listar Departamentos

--- src/main/java/com/edercatini/springrevisao/web/controller/DepartamentoController.java
+++ src/main/java/com/edercatini/springrevisao/web/controller/DepartamentoController.java
@@ -2,6 +2,7 @@ package com.edercatini.springrevisao.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class DepartamentoController {
 	private DepartamentoService departamentoService;
 
 	@Autowired
-	public DepartamentoController(DepartamentoService departamentoService) {
+	DepartamentoController(DepartamentoService departamentoService) {
 		this.departamentoService = departamentoService;
 	}
 
@@ -26,7 +27,8 @@ public class DepartamentoController {
 	}
 
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", departamentoService.encontrarTodos());
 		return "/departamento/lista";
 	}
 
